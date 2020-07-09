c61d29fbc02ce4553e5994c9e1bca9ecb5630468
iliess-np <59144806+iliess-np@users.noreply.github.com>
2020-07-09 13:07:17 +0200

    finale edit

---
+++ Task 6 Flying Rocket with Flames cssd-js/app.js
@@ -0,0 +1,19 @@
+function more() {
+  let i = 0;
+  while (i < 100) {
+    const container = document.querySelector(".container");
+    let stars = document.createElement("span");
+    let posleft = Math.floor(Math.random() * window.innerWidth);
+    let height = Math.floor(Math.random() * 100);
+
+    stars.style.left = posleft + "px";
+    stars.style.height = 100 + height + "px";
+
+    let duration = Math.random() * 1;
+    stars.style.animationDuration = duration + "s";
+
+    container.appendChild(stars);
+    i++;
+  }
+}
+more();
