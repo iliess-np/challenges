8cd004cd13ba28302fd8231ecff8267977dd5bc2
iliess-np <59144806+iliess-np@users.noreply.github.com>
2020-07-19 21:19:27 +0200

    .

--- Task 7 WebGL Liquid Distortion Image/app.js
+++ Task 7 WebGL Liquid Distortion Image/app.js
@@ -5,6 +5,6 @@ Array.from(document.querySelectorAll(".imgBox")).forEach((e) => {
     intensity: 0.3,
     image1: imgs[0].getAttribute("src"),
     image2: imgs[1].getAttribute("src"),
-    displacementImage: "./images/displacement/7.jpg",
+    displacementImage: "./images/displacement/6.jpg",
   });
 });
